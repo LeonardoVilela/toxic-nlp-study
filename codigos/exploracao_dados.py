59dd9ab56d39b87922d7ee254a9fa02c944f81e1
LeonardoVilela <53622663+LeonardoVilela@users.noreply.github.com>
2023-09-11 14:31:43 -0300

    Update exploracao_dados.py

--- codigos/exploracao_dados.py
+++ codigos/exploracao_dados.py
@@ -40,7 +40,7 @@ class BertTokenizer(object):
         attention_mask = np.where(padded != 0, 1, 0)
         input_ids = torch.tensor(padded,device = device)
         attention_mask = torch.tensor(attention_mask,device = device)
-        with torch.no_grad(): last_hidden_states = self.model(input_ids, attention_mask=attention_mask,device = device)
+        with torch.no_grad(): last_hidden_states = self.model(input_ids, attention_mask=attention_mask)
         features = last_hidden_states[0][:, 0, :].numpy()
         return features
 
