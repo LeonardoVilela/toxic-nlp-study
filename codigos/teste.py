4329c853bd0a67292676ceea815d652e764884d1
Leonardo Vilela Pereira <53622663+LeonardoVilela@users.noreply.github.com>
2023-09-28 10:58:56 -0300

    Update teste.py

--- codigos/teste.py
+++ codigos/teste.py
@@ -21,7 +21,7 @@ import xgboost
 import pickle
 import joblib
 
-# patch_sklearn()
+patch_sklearn()
 
 # device = xm.xla_device()
 # torch.arange(0, 100, device=device)
@@ -83,9 +83,9 @@ def told_dataset(tamanho):
 df_told,df_full = told_dataset(5000)
 
 def train_tokens(df_full):
-    # _instance =BertTokenizer(text=list(df_full['text']))
-    # X_train = _instance.get()
-    X_train = np.loadtxt('X_train.txt')
+    _instance =BertTokenizer(text=list(df_full['text']))
+    X_train = _instance.get()
+    # X_train = np.loadtxt('X_train.txt')
     print(X_train.shape)
     print("X_train acquired")
     y_train = df_full['bin_class']#.iloc[0:250]
@@ -103,9 +103,9 @@ def teste_tokens(df_told,tamanho):
     df_full.reset_index(inplace=True)
     y_test = df_full['bin_class']#.iloc[250:375]
     
-    # _instance =BertTokenizer(text=list(df_full['text']))
-    # X_test = _instance.get()
-    X_test = np.loadtxt('X_test.txt')
+    _instance =BertTokenizer(text=list(df_full['text']))
+    X_test = _instance.get()
+    # X_test = np.loadtxt('X_test.txt')
     print(X_test.shape)
     print("X_test acquired")
     return X_test,y_test
