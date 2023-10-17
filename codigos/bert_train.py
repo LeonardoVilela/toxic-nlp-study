853a70697a6f5be79c7b3431cd1b6b4bcfd4e7b9
Leonardo Vilela Pereira <53622663+LeonardoVilela@users.noreply.github.com>
2023-10-16 22:40:20 -0300

    Update bert_train.py

--- codigos/bert_train.py
+++ codigos/bert_train.py
@@ -71,7 +71,7 @@ tokenizer = AutoTokenizer.from_pretrained("../bert-base-portuguese-cased")
 
 try:
     tpu = tf.distribute.cluster_resolver.TPUClusterResolver()#'grpc://10.164.0.30')
-    print('Running on TPU ', tpu.cluster_spec().as_dict()['worker'])  
+    print('Running on TPU ', tpu.cluster_spec().as_dict())  
     tf.config.experimental_connect_to_cluster(tpu)
     tf.tpu.experimental.initialize_tpu_system(tpu)
     strategy = tf.distribute.experimental.TPUStrategy(tpu)
