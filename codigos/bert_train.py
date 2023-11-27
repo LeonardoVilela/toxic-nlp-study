84063ab383e7f44b6cc13189ab494418f94a98f8
Leonardo Vilela Pereira <53622663+LeonardoVilela@users.noreply.github.com>
2023-11-27 16:58:54 -0300

    Update bert_train.py

--- codigos/bert_train.py
+++ codigos/bert_train.py
@@ -94,8 +94,8 @@ for i in range(len(df_told_full)):
 print(np.unique(y))
 df_told_full['bin_class'] = y
 
-df_sub_a = df_told_full[df_told_full['bin_class']==0].iloc[0:500]
-df_sub_b = df_told_full[df_told_full['bin_class']==1].iloc[0:500]
+df_sub_a = df_told_full[df_told_full['bin_class']==0].iloc[0:50]
+df_sub_b = df_told_full[df_told_full['bin_class']==1].iloc[0:50]
 df_balanced_told = pd.concat([df_sub_a,df_sub_b])
 
 def preprocess_text(text):
@@ -114,10 +114,10 @@ def preprocess_text(text):
 df_balanced_told['text'] = df_balanced_told['text'].apply(preprocess_text)
 dataset = tf.data.Dataset.from_tensor_slices((df_balanced_told['text'],df_balanced_told['bin_class']))
 size = dataset.cardinality().numpy()
-train_size = int(0.85*size)
-test_size = int(size-train_size)
+train_size = int(1*size)
+# test_size = int(size-train_size)
 ds_train = dataset.take(train_size)
-ds_test = dataset.skip(train_size)
+# ds_test = dataset.skip(train_size)
 
 max_length = 512
 batch_size = 256
@@ -153,13 +153,10 @@ def encode_examples(ds, limit=-1):
     return tf.data.Dataset.from_tensor_slices((input_ids_list, attention_mask_list, token_type_ids_list, label_list)).map(map_example_to_dict)
 
 # train dataset
+
 start=time.time()
 ds_train_encoded = encode_examples(ds_train).shuffle(10000).batch(batch_size)
 print("Done with Training Dataset",time.time()-start)
-# test dataset
-start=time.time()
-ds_test_encoded = encode_examples(ds_test).batch(batch_size)
-print("Done with Testing Dataset",time.time()-start)
 
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.callbacks import ModelCheckpoint
@@ -186,44 +183,115 @@ def scheduler(epoch, lr):
     return lr
   else:
     return lr * 0.5#tf.math.exp(-0.1)
-input_ids = tf.keras.Input(shape=(max_length,),dtype='int32',name = 'input_ids')
-attention_mask = tf.keras.Input(shape=(max_length,),dtype='int32',name = 'attention_mask')
-token_type_ids = tf.keras.Input(shape=(max_length,),dtype='int32',name = 'token_type_ids')
+# input_ids = tf.keras.Input(shape=(max_length,),dtype='int32',name = 'input_ids')
+# attention_mask = tf.keras.Input(shape=(max_length,),dtype='int32',name = 'attention_mask')
+# token_type_ids = tf.keras.Input(shape=(max_length,),dtype='int32',name = 'token_type_ids')
 
-output = model_tf([input_ids,attention_mask,token_type_ids])
-output = output[0]
-output = tf.keras.layers.Dense(128,activation = gelu)(output)
-output = tf.keras.layers.Dropout(0.1)(output)
-output = tf.keras.layers.Dense(256,activation = gelu)(output)
-output = tf.keras.layers.Dropout(0.2)(output)
-output = tf.keras.layers.Dense(512,activation = gelu)(output)
-output = tf.keras.layers.Dropout(0.2)(output)
-output = tf.keras.layers.Dense(1,activation = 'softmax')(output)
+# output = model_tf([input_ids,attention_mask,token_type_ids])
+# output = output[0]
+# output = tf.keras.layers.Dense(128,activation = gelu)(output)
+# output = tf.keras.layers.Dropout(0.1)(output)
+# output = tf.keras.layers.Dense(256,activation = gelu)(output)
+# output = tf.keras.layers.Dropout(0.2)(output)
+# output = tf.keras.layers.Dense(512,activation = gelu)(output)
+# output = tf.keras.layers.Dropout(0.2)(output)
+# output = tf.keras.layers.Dense(1,activation = 'softmax')(output)
 
-model = tf.keras.models.Model(inputs = [input_ids,attention_mask,token_type_ids],outputs = output)
-# choosing Adam optimizer
-optimizer = tf.keras.optimizers.Adam(learning_rate=lr, epsilon=1e-07)
-optimizer2 = tf.keras.optimizers.AdamW(learning_rate=lr,weight_decay=0.004,epsilon=1e-07)
-# we do not have one-hot vectors, we can use sparce categorical cross entropy and accuracy
-loss = tf.keras.losses.BinaryFocalCrossentropy()
-metric = tf.keras.metrics.BinaryAccuracy('acc')
-early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=300)
-csv_log = tf.keras.callbacks.CSVLogger('training.log')
-#callbacks=[early_stopping]
-callbacks_lr = tf.keras.callbacks.LearningRateScheduler(scheduler)
-checkpoint_filepath = 'modelo_bert_current.h5'
-model_checkpoint_callback = ModelCheckpoint(
-    filepath=checkpoint_filepath,
-    monitor='val_loss',
-    save_best_only=False,
-    save_weights_only=False,
-    mode='min',
-    save_freq='epoch',
-    period=3
-)
-model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
-bert_history = model.fit(ds_train_encoded, epochs=15, validation_data=ds_test_encoded, callbacks=[csv_log,model_checkpoint_callback,early_stopping])
-model.save('modelo_bert_2000_adam_2voters.h5')
-model.compile(optimizer=optimizer2, loss=loss, metrics=[metric])
-bert_history = model.fit(ds_train_encoded, epochs=15, validation_data=ds_test_encoded, callbacks=[csv_log,model_checkpoint_callback,early_stopping])
-model.save('modelo_bert_2000_adamw_2voters.h5')
+# model = tf.keras.models.Model(inputs = [input_ids,attention_mask,token_type_ids],outputs = output)
+# # choosing Adam optimizer
+# optimizer = tf.keras.optimizers.Adam(learning_rate=lr, epsilon=1e-07)
+# optimizer2 = tf.keras.optimizers.AdamW(learning_rate=lr,weight_decay=0.004,epsilon=1e-07)
+# # we do not have one-hot vectors, we can use sparce categorical cross entropy and accuracy
+# loss = tf.keras.losses.BinaryFocalCrossentropy()
+# metric = tf.keras.metrics.BinaryAccuracy('acc')
+# early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_acc', patience=300)
+# csv_log = tf.keras.callbacks.CSVLogger('training.log')
+# #callbacks=[early_stopping]
+# callbacks_lr = tf.keras.callbacks.LearningRateScheduler(scheduler)
+# checkpoint_filepath = 'modelo_bert_current.h5'
+# model_checkpoint_callback = ModelCheckpoint(
+#     filepath=checkpoint_filepath,
+#     monitor='val_loss',
+#     save_best_only=False,
+#     save_weights_only=False,
+#     mode='min',
+#     save_freq='epoch',
+#     period=3
+# )
+# model.compile(optimizer=optimizer, loss=loss, metrics=[metric])
+# bert_history = model.fit(ds_train_encoded, epochs=15, validation_data=ds_test_encoded, callbacks=[csv_log,model_checkpoint_callback,early_stopping])
+# model.save('modelo_bert_2000_adam_2voters.h5')
+# model.compile(optimizer=optimizer2, loss=loss, metrics=[metric])
+# bert_history = model.fit(ds_train_encoded, epochs=15, validation_data=ds_test_encoded, callbacks=[csv_log,model_checkpoint_callback,early_stopping])
+# model.save('modelo_bert_2000_adamw_2voters.h5')
+model = tf.keras.models.load_model("modelo_bert_2000_adamw_2voters.h5")
+y_pred = model.predict(X_test)
+print(y_pred.shape)
+print('Precision: %.3f' % precision_score(y_test, y_pred))
+print('Recall: %.3f' % recall_score(y_test, y_pred))
+print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+print('F1 Score: %.3f' % f1_score(y_test, y_pred))
+
+conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+plt.figure(figsize = (10,7))
+sns.heatmap(conf_matrix, annot=True)
+
+ax =sns.heatmap(conf_matrix, annot=True)
+
+# save the plot as PDF file
+plt.savefig("confusionmatrix_told_bert.png", format='png')
+def hate_dataset():
+    df_hate = pd.read_csv('../gportuguese_hate_speech_binary_classification.csv')
+    print("df_hate acquired")
+    counts = Counter(df_hate['hatespeech_comb'])
+    print(f'full hate: {counts}')
+    return df_hate
+df_hate = hate_dataset
+df_sub_hate = df_hate.iloc[0:100,:]
+counts = Counter(df_sub_hate['hatespeech_comb'])
+print(f"sub: {counts}")
+
+dataset = tf.data.Dataset.from_tensor_slices((df_sub_hate['text'],df_sub_hate['hatespeech_comb']))
+size = dataset.cardinality().numpy()
+train_size = int(1*size)
+# test_size = int(size-train_size)
+ds_train = dataset.take(train_size)
+# ds_test = dataset.skip(train_size)
+start=time.time()
+ds_train_encoded = encode_examples(ds_train).shuffle(10000).batch(batch_size)
+print("Done with Training Dataset",time.time()-start)
+
+
+y_pred = clf.predict(ds_train_encoded)
+
+
+print('Precision: %.3f' % precision_score(y_test, y_pred))
+print('Recall: %.3f' % recall_score(y_test, y_pred))
+print('Accuracy: %.3f' % accuracy_score(y_test, y_pred))
+print('F1 Score: %.3f' % f1_score(y_test, y_pred))
+
+conf_matrix = confusion_matrix(y_true=y_test, y_pred=y_pred)
+#plt.figure(figsize = (10,7))
+ax =sns.heatmap(conf_matrix, annot=True)
+
+# save the plot as PDF file
+plt.savefig("confusionmatrix_hate_LSTM_rs.png", format='png')
+
+X_test_hate_sub = list(df_sub_hate['text'])
+_instance =BertTokenizer(text=X_test_hate_sub)
+X_test_hate_sub = _instance.get()
+y_test_sub = list(df_sub_hate['hatespeech_comb'])
+
+y_pred = clf.predict(X_test_hate_sub)
+
+print('Precision: %.3f' % precision_score(y_test_sub, y_pred))
+print('Recall: %.3f' % recall_score(y_test_sub, y_pred))
+print('Accuracy: %.3f' % accuracy_score(y_test_sub, y_pred))
+print('F1 Score: %.3f' % f1_score(y_test_sub, y_pred))
+
+conf_matrix = confusion_matrix(y_true=y_test_sub, y_pred=y_pred)
+#plt.figure(figsize = (10,7))
+ax =sns.heatmap(conf_matrix, annot=True)
+
+# save the plot as PDF file
+plt.savefig("confusionmatrix_hate_sub1000_LSTM_rs.png", format='png')
