aa7d2ffe3c19eed5505429f1cd7f2ba25f0321a5
Leonardo Vilela Pereira <53622663+LeonardoVilela@users.noreply.github.com>
2023-11-27 12:24:33 -0300

    Update lstm.py

--- codigos/lstm.py
+++ codigos/lstm.py
@@ -124,13 +124,13 @@ clf.add(LSTM(30, activation='tanh', input_shape=(X_test.shape[1],1)))
 clf.add(Dense(units=64, activation='tanh'))
 clf.add(Dense(units=128, activation='tanh'))
 clf.add(Dense(1,activation = 'softmax'))
-clf.compile(loss='BinaryFocalCrossentropy', optimizer='adam',metrics = [tf.keras.metrics.F1Score(name = 'f1')])
+clf.compile(loss='BinaryFocalCrossentropy', optimizer='adam',metrics = [tf.keras.metrics.Accuracy(name = 'acc')])
 train = tf.data.Dataset.from_tensor_slices((X_train.astype(np.float32), y_train.astype(np.float32)))
 train = train.batch(64)
 
 test_dataset = tf.data.Dataset.from_tensor_slices((X_test.astype(np.float32), y_test.astype(np.float32)))
 test_dataset = test_dataset.batch(64)
-clf.fit(train, epochs=50, batch_size=64,validation_data=test_dataset,callbacks=tf.keras.callbacks.EarlyStopping(monitor='f1', patience=50))
+clf.fit(train, epochs=15, batch_size=64,validation_data=test_dataset)
 
 # save model
 # clf = pickle.load(open('rf_model_nors.pickle', 'rb'))
